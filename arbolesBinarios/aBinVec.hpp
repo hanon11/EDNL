4ecdd2e4538a636fe7178513fe11ed3c522447c6
hanon11 <lucia.atienza2@gmail.com>
2021-02-22 13:09:38 +0100

    implementaciones

--- arbolesBinarios/aBinVec.hpp
+++ arbolesBinarios/aBinVec.hpp
@@ -1,6 +1,7 @@
 #ifndef ABIN_VEC_HPP
 #define ABIN_VEC_HPP
 #include <cassert>
+#include <cstdint>
 
 template<typename T> 
 class AbinVec
@@ -35,6 +36,97 @@ private:
     std::size_t numNodos; //numero de nodos del arbol
 
 };
+
+
+//definicion de nodo nulo
+template <typename T>
+const typename AbinVec<T>::nodo AbinVec<T>::NODO_NULO(SIZE_MAX);
+
+template <typename T>
+inline AbinVec<T>::AbinVec(std::size_t maxNodos) :
+    nodos(new celda[maxNodos]),
+    maxNodos(maxNodos),
+    numNodos(0)
+{}
+
+
+template <typename T>
+inline void AbinVec<T>::insertarRaiz(const T& e)
+{
+    assert(numNodos == 0);
+
+    numNodos = 1;
+    nodos[0].elto = e;
+    nodos[0].padre = NODO_NULO;
+    nodos[0].hizq = NODO_NULO;
+    nodos[0].hder = NODO_NULO;
+}
+
+template <typename T>
+inline void AbinVec<T>::insertarHijoIzqdo(nodo n, const T& e)
+{
+    assert(n >= 0 && n < numNodos); //nodo valido
+    assert(nodos[n].hizq == NODO_NULO); //n no tiene hijo izq
+    assert(numNodos < maxNodos); // arbol no lleno
+
+    //add new nodo
+    nodos[n].hizq = numNodos;
+    nodos[numNodos].elto = e;
+    nodos[numNodos].padre = n;
+    nodos[numNodos].hizq = NODO_NULO;
+    nodos[numNodos].hder = NODO_NULO;
+    ++numNodos;
+}
+
+
+template <typename T>
+inline void AbinVec<T>::insertarHijoDrcho(nodo n, const T& e)
+{
+    assert(n >= 0 && n < numNodos); //nodo valido
+    assert(nodos[n].hder == NODO_NULO);//n no tiene hijo drch
+    assert(numNodos < maxNodos); //arbol no lleno
+    
+    //add new nodo
+    nodos[n].hder = numNodos;
+    nodos[numNodos].elto = e;
+    nodos[numNodos].padre = n;
+    nodos[numNodos].hizq = NODO_NULO;
+    nodos[numNodos].hder = NODO_NULO;
+    ++numNodos;
+ }
+
+template <typename T>
+void AbinVec<T>::eliminarHijoIzqdo(nodo n)
+{
+    nodo hizqdo;
+    assert(n >= 0 && n < numNodos);//nodo valido
+    hizqdo = nodos[n].hizq;
+    assert(hizqdo != NODO_NULO);
+    assert(nodos[hizqdo].hizq == NODO_NULO && nodos[hizqdo].hder == NODO_NULO);
+    //hijo izq de n es hoja
+
+    if(hizqdo!= numNodos-1)
+    {// Mover el último nodo a la posición del hijo izqdo.
+        nodos[hizqdo] = nodos[numNodos-1];// Actualizar la posición del hijo (izquierdo o derecho) en el padre del nodo movido.
+        if(nodos[nodos[hizqdo].padre].hizq == numNodos-1)
+            nodos[nodos[hizqdo].padre].hizq = hizqdo;
+        else
+            nodos[nodos[hizqdo].padre].hder= hizqdo;
+        // Si el nodo movido tiene hijos, actualizar la posición del padre.
+        if(nodos[hizqdo].hizq != NODO_NULO)
+            nodos[nodos[hizqdo].hizq].padre = hizqdo;
+        if(nodos[hizqdo].hder!= NODO_NULO)
+            nodos[nodos[hizqdo].hder].padre = hizqdo;
+    }
+    
+    nodos[n].hizq= NODO_NULO;
+    --numNodos;
+}
+ 
+ 
+
+
+
 
 
 #endif
