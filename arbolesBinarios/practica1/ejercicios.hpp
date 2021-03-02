5c4090382824f16b739d1399df0ab74c8bfe44f2
hanon11 <lucia.atienza2@gmail.com>
2021-03-02 21:02:44 +0100

    practica 1

---
+++ arbolesBinarios/practica1/ejercicios.hpp
@@ -0,0 +1,55 @@
+#include "../aBinE-S.h"
+#include "../aBinEnlazada.hpp"
+
+//EJERCICIO1
+template<typename T>
+unsigned numNodos(const Abin<T>& A){
+
+    return numNodos_Rec(A.raiz(), A);
+}
+
+template <typename T>
+unsigned numNodos_Rec(typename Abin<T>::nodo n, const Abin<T>& A){
+
+    if(n == Abin<T>::NODO_NULO){
+
+        return 0;
+    }
+    else{
+        
+        return 1 + numNodos_Rec(A.hijoDrcho(n), A) + numNodos_Rec(A.hijoIzqdo(n), A);
+    }
+}
+
+
+//EJERCICIO2
+template <typename T>
+int altura(Abin<T>& A)
+{
+    return alturaRec(A.raiz(), A);
+}
+
+template <typename T>
+int alturaRec(typename Abin<T>::nodo n, Abin<T>& A)
+{
+    if(n == Abin<T>::NODO_NULO)
+        return -1;
+    else 
+        return 1 + std::max(alturaRec(A.hijoIzqdo(n), A), alturaRec(A.hijoDrcho(n), A));
+}
+
+
+//EJERCICIO3
+template <typename T>
+int profundidad(typename Abin<T>::nodo n, Abin<T>& A)
+{
+    int profundidad = 0;
+
+    while (n != A.raiz())
+    {
+        profundidad++;
+        n = A.padre(n);
+    }
+
+    return profundidad;
+}
---
+++ arbolesBinarios/practica1/prueba.cpp
@@ -0,0 +1,22 @@
+#include "../aBinE-S.h"
+#include "../aBinEnlazada.hpp"
+#include "ejercicios.hpp"
+#include <iostream>
+#include <fstream>
+
+int main(){
+
+    Abin<char> A;
+
+    std::cout << "**LECTURA DE ARBOL BINARIO FICHERO DE DATOS**" << std::endl;
+    std::ifstream fe("abin.dat"); //abre fichero de entrada
+    rellenarAbin(fe, A);
+    fe.close();
+
+    std::cout << "MOSTRAR ARBOL BINARIO B" << std::endl;
+    imprimirAbin(A);
+
+    std::cout << "El arbol tiene " << numNodos(A) << " nodos "<< std::endl;
+    std::cout << "La altura del arbol es " << altura(A) << std::endl;
+    std::cout << "La profundidad del arbol es " << profundidad(A.hijoDrcho(A.raiz()), A) << std::endl;
+}
