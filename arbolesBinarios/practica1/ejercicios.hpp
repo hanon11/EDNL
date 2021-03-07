34b9413858bbcf8236f464048ffee467c56cd97a
hanon11 <lucia.atienza2@gmail.com>
2021-03-07 14:16:51 +0100

    practica 1

--- arbolesBinarios/practica1/ejercicios.hpp
+++ arbolesBinarios/practica1/ejercicios.hpp
@@ -24,7 +24,7 @@ unsigned numNodos_Rec(typename Abin<T>::nodo n, const Abin<T>& A){
 
 //EJERCICIO2
 template <typename T>
-int altura(Abin<T>& A)
+int altura(const Abin<T>& A)
 {
     return alturaRec(A.raiz(), A);
 }
@@ -41,7 +41,7 @@ int alturaRec(typename Abin<T>::nodo n, Abin<T>& A)
 
 //EJERCICIO3
 template <typename T>
-int profundidad(typename Abin<T>::nodo n, Abin<T>& A)
+int profundidad(typename Abin<T>::nodo n, const Abin<T>& A)
 {
     int profundidad = 0;
 
@@ -55,10 +55,32 @@ int profundidad(typename Abin<T>::nodo n, Abin<T>& A)
 }
 //forma recursiva
 template <typename T>
-int profundidadRec(typename Abin<T>::nodo n, Abin<T>& A)
+int profundidadRec(typename Abin<T>::nodo n, const Abin<T>& A)
 {
     if (n == Abin<T>::NODO_NULO)
         return -1;
     else 
         return 1 + profundidadRec(A.padre(n), A);
+}
+
+
+
+//EJERCICIO 6
+template <typename T>
+int desequilibrio(const Abin<T>& A)
+{
+    if (A.arbolVacioB())
+        return 0;
+    else
+        return desequilibrio_rec(A, A.raizB());
+}
+
+
+template <typename T>
+int desequilibrio_rec(const Abin<T> &A, typename Abin<T>::nodo n)
+{
+    if (nA == Abin<T>::NODO_NULO)
+        return 0;
+    else
+        return std::max(std::abs(A.alturaB(A.hijoDrchoB(A)) - A.alturaB(A.hijoIzqdoB(A))), std::max(desequilibrio_rec(A, A.hijoDrchoB(A)),desequilibrio_rec(A,A.hijoIzqdoB(A))));
 }
