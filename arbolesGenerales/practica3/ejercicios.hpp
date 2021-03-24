046e1410a5ade69fa872ddade2df1c5f95491909
hanon11 <lucia.atienza2@gmail.com>
2021-03-24 22:30:42 +0100

    practica 3

--- arbolesGenerales/practica3/ejercicios.hpp
+++ arbolesGenerales/practica3/ejercicios.hpp
@@ -75,3 +75,52 @@ int profundidadRec(typename Agen<T>::nodo n, const Agen<T>& A)
     else 
         return 1 + profundidadRec(A.padre(n), A);
 }
+
+//EJERCICIO 4
+void borrarDescendencia(Agen<int>::nodo n, Agen<int>& A)
+{
+    Agen<int>::nodo hijo = A.hijoIzqdo(n);
+
+    if (n != Agen<int>::NODO_NULO)
+    {
+        while (hijo != Agen<int>::NODO_NULO && A.hijoIzqdo(hijo) == Agen<int>::NODO_NULO)
+        {
+            A.eliminarHijoIzqdo(n);
+            hijo = A.hijoIzqdo(n);
+        }
+        if (hijo != Agen<int>::NODO_NULO && A.hijoIzqdo(hijo) != Agen<int>::NODO_NULO)
+        {
+            borrarDescendencia(hijo, A);
+            A.eliminarHijoIzqdo(n);
+        }
+    }
+}
+
+void podaRec(Agen<int>::nodo n, Agen<int>& A, int x)
+{
+    bool encontrado = false;
+    Agen<int>::nodo hijo = A.hijoIzqdo(n);
+    if(n != Agen<int>::NODO_NULO && !encontrado)
+    {
+        if(A.elemento(n) == x)
+        {
+            encontrado = true;
+            borrarDescendencia(n, A);
+        }
+        else
+        {
+            hijo = A.hijoIzqdo(n);
+            while(hijo != Agen<int>::NODO_NULO)
+            {
+                podaRec(hijo, A, x);
+                hijo = A.hermDrcho(hijo);
+            }
+        }
+    }
+}
+
+void poda(int x, Agen<int>& A)
+{
+    return podaRec(A.raiz(), A, x);
+}
+
