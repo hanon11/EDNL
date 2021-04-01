586d2885ed8f59fb1e0f6572f4e283a907b4abda
hanon11 <lucia.atienza2@gmail.com>
2021-04-01 20:03:10 +0200

    extra

--- arbolesGenerales/extra/3nietos.hpp
+++ arbolesGenerales/extra/3nietos.hpp
@@ -1,20 +1,28 @@
 #include "../aGenEnlazada.hpp"
 template <typename T>
-unsigned nietos(typename Agen<T>::nodo n, const Agen<T>& A)
+unsigned nietos(const Agen<T>& A)
+{
+    return nietosRec(A.raiz(), A);
+}
+
+template <typename T>
+unsigned nietosRec(typename Agen<T>::nodo n, const Agen<T>& A)
 {
     unsigned numNietos = 0;
     typename Agen<T>::nodo hijo = A.hijoIzqdo(n);
+    bool encontrado = false;
     if(n != Agen<T>::NODO_NULO && hijo != Agen<T>::NODO_NULO)
-    {   
-        if(contarHijos(hijo, A) == 3)
-            numNietos++;
+    {
         while(hijo != Agen<T>::NODO_NULO)
         {
-            nietos(hijo, A);
-            if(contarHijos(hijo, A) == 3)
+            if(contarHijos(hijo, A) == 3 && !encontrado)
+            {
                 numNietos++;
+                encontrado = true;
+            }
+            numNietos += nietosRec(hijo, A);
             hijo = A.hermDrcho(hijo);
-        }    
+        }
     }
     return numNietos;
 }
