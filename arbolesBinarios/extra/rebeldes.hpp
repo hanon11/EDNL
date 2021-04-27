c1fb391cde5afe1109eb63a7c388dfc52a740d24
hanon11 <lucia.atienza2@gmail.com>
2021-04-27 20:45:28 +0200

    extra

---
+++ arbolesBinarios/extra/rebeldes.hpp
@@ -0,0 +1,61 @@
+#include "../aBinEnlazada.hpp"
+
+unsigned ascendencia1(typename Abin<int>::nodo n, const Abin<int>& A)
+{
+    if(A.padre(n) == Abin<int>::NODO_NULO)
+        return 0;
+    else
+    {
+        if(A.elemento(A.padre(n)) == 1)
+            return 1 + ascendencia1(A.padre(n), A);
+        else
+            return 0 + ascendencia1(A.padre(n), A);
+    }
+}
+
+
+unsigned ascendencia0(typename Abin<int>::nodo n, const Abin<int>& A)
+{
+    if(A.padre(n) == Abin<int>::NODO_NULO)
+        return 0;
+    else
+    {
+        if(A.elemento(A.padre(n)) == 0)
+            return 1 + ascendencia1(A.padre(n), A);
+        else
+            return 0 + ascendencia1(A.padre(n), A);
+    }
+}
+
+bool rebelde(typename Abin<int>::nodo n, const Abin<int>& A)
+{
+    if(A.elemento(n) == 1 && ascendencia1(n, A) < ascendencia0(n, A))
+        return true;
+    else
+    {
+        if(A.elemento(n) == 0 && ascendencia1(n, A) > ascendencia0(n, A))
+            return true;
+        else 
+            return false;
+    }
+}
+
+
+unsigned rebeldesRec(typename Abin<int>::nodo n, const Abin<int>& A)
+{
+    if(n == Abin<int>::NODO_NULO)
+        return 0;
+    else
+    {
+        if (rebelde(n, A))
+            return 1 + rebeldesRec(A.hijoDrcho(n), A) + rebeldesRec(A.hijoIzqdo(n), A);
+        else
+            return 0 + rebeldesRec(A.hijoDrcho(n), A) + rebeldesRec(A.hijoIzqdo(n), A);
+    }
+}
+unsigned rebeldes(const Abin<int>& A)
+{
+    return rebeldesRec(A.raiz(), A);
+}
+
+
